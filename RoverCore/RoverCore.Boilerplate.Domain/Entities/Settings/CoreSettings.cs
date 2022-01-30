8a3f5f1376701649a024ce1f295440452d737aef
Michael Tanczos <mike@logic-gate.com>
2022-01-30 18:23:50 -0500

    Added site configuration admin panel

--- RoverCore/RoverCore.Boilerplate.Domain/Entities/Settings/CoreSettings.cs
+++ RoverCore/RoverCore.Boilerplate.Domain/Entities/Settings/CoreSettings.cs
@@ -1,4 +1,6 @@
-﻿namespace RoverCore.Boilerplate.Domain.Entities.Settings
+﻿using System.ComponentModel;
+
+namespace RoverCore.Boilerplate.Domain.Entities.Settings
 {
     /// <summary>
     /// Core strongly-typed application settings for this web service
@@ -9,10 +11,12 @@
         /// <summary>
         /// Entity Framework migrations can be applied when the application starts up if true
         /// </summary>
+        [DisplayName("Apply Migrations On Startup")]
         public bool ApplyMigrationsOnStartup { get; set; } = true;
         /// <summary>
         /// Any ISeeder seeders in the project will run on startup if true
         /// </summary>
+        [DisplayName("Seed Data On Startup")]
         public bool SeedDataOnStartup { get; set; } = true;
     }
 
