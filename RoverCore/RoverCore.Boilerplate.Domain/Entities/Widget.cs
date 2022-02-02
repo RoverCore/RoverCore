1595b2e0f45978d970e271ecb059c7a0f882dd77
Michael Tanczos <tanczosm@eastonsd.org>
2022-02-02 09:54:17 -0500

    Added widget class for testing

---
+++ RoverCore/RoverCore.Boilerplate.Domain/Entities/Widget.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RoverCore.Boilerplate.Domain.Entities
+{
+    public class Widget
+    {
+        [Key]
+        public int WidgetId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool Available { get; set; }
+        public int Count { get; set; }
+        public double Price { get; set; }
+    }
+}
