6100520b279d56b5a04a9ccca04709cf80da3a2b
Michael Tanczos <mike@logic-gate.com>
2022-02-06 18:51:00 -0500

    Simplified job addition

--- RoverCore/RoverCore.Boilerplate.Web/Jobs/ConfigureJobs.cs
+++ RoverCore/RoverCore.Boilerplate.Web/Jobs/ConfigureJobs.cs
@@ -2,7 +2,7 @@
 
 namespace RoverCore.Boilerplate.Web.Jobs
 {
-    public static class JobConfiguration
+    public static class ConfigureJobs
     {
         /// <summary>
         /// Schedule all your recurring Hangfire jobs in this method.  This will be called at the end of the application middleware initialization.
@@ -10,7 +10,7 @@ namespace RoverCore.Boilerplate.Web.Jobs
 	    public static void Schedule()
 	    {
             // Sample job that prints hello every minute to the console
-		    RecurringJob.AddOrUpdate<ISampleJob>(generator => generator.Hello(), "* * * * *");
+		    RecurringJob.AddOrUpdate<SampleJob>(generator => generator.Hello(), "* * * * *");
 	    }
     }
 }
--- RoverCore/RoverCore.Boilerplate.Web/Jobs/SampleJob.cs
+++ RoverCore/RoverCore.Boilerplate.Web/Jobs/SampleJob.cs
@@ -5,16 +5,11 @@ using Serviced;
 
 namespace RoverCore.Boilerplate.Web.Jobs
 {
-	public interface ISampleJob
-	{
-		void Hello();
-	}
-
 	/// <summary>
 	/// This is a sample job for Hangfire.  Note that this job is scheduled in the JobConfiguration.cs class and it's service registration is automatically
-	/// performed by adding the IScoped<ISampleJob> interface.  If you fail to add this interface you will need to register the service manually in Startup.cs
+	/// performed by adding the IScoped interface.  If you fail to add this interface you will need to register the service manually in Startup.cs
 	/// </summary>
-    public class SampleJob : ISampleJob, IScoped<ISampleJob>
+    public class SampleJob : IScoped
     {
 	    public void Hello()
 	    {
