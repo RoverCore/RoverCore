dcaa0f4d035b5a549c5f94ba8c68f7bea8a386b0
Michael Tanczos <tanczosm@eastonsd.org>
2022-03-15 13:13:10 -0400

    Reverted to asp.net 6.0.2 libraries (6.0.3 code generation library was not released)

--- RoverCore.Boilerplate.Infrastructure/Persistence/Migrations/ApplicationDbContextModelSnapshot.cs
+++ RoverCore.Boilerplate.Infrastructure/Persistence/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -17,7 +17,7 @@ namespace RoverCore.Boilerplate.Infrastructure.Persistence.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "6.0.3")
+                .HasAnnotation("ProductVersion", "6.0.2")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder, 1L, 1);
